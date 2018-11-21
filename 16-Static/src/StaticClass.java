f6546b34105b59aded2947fc91d537c8d7dac2f1
Aliya <aliyakali01@gmail.com>
2018-11-21 13:28:15 -0500

    JavaProject  git push from eclipse)))

---
+++ 16-Static/src/StaticClass.java
@@ -0,0 +1,9 @@
+public class StaticClass {
+	
+	public static void main(String[]args) {
+		StaticTest static = new StaticTest("instance-1");
+		System.out.println(staitc1.getName()+" is instance number " + static1.getnumInstance());
+		}
+	}
+
+}
